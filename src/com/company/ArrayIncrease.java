e32808398e585a42d26d8aa19f5b36453dfb1621
Vahza <waja11@mail.ru>
2021-09-26 00:17:51 +0300

    Add Increase and Compare(25.09.2021)

--- src/com/company/ArrayIncrease.java
+++ src/com/company/ArrayIncrease.java
@@ -1,17 +1,22 @@
 package com.company;
 
 public class ArrayIncrease {
-    private static int[] IncreaseArray(int[] array) {
-        int lenght = array.length;
-        int[] secondArray = new int[array.length * 2];
+    private static int[] IncreaseArray(int[] array,int length) {
+        int[] secondArray = new int[length ];
+        for (int i = 0; i <secondArray.length ; i++) {
+
+
+        }
+
         return secondArray;
     }
 
     public static void main(String[] args) {
         int[] testArray = new int[]{3, -32, 12, -65, 324, 34, 1, 123, 34};
-        int[] value = IncreaseArray(testArray);
-        for (int i = 0; i < value.length; i++) {
-            System.out.println(value[i]);
+        int[] value = IncreaseArray(testArray,20);
+        for (int i:value)
+        {
+            System.out.println(i);
         }
         System.out.println(value.length);
     }
--- src/com/company/CompareArrays.java
+++ src/com/company/CompareArrays.java
@@ -1,18 +1,15 @@
 package com.company;
 
 public class CompareArrays {
-
-    private static boolean Compare(int[] firstArray, int[] secondArray) {
+    private static boolean compareArrays(int[] firstArray, int[] secondArray) {
         if (firstArray == secondArray) {
             return true;
-        } //если  равны массивы возвращаем правду
-
-        int length = secondArray.length; //переменная длины массива
-        if (length != firstArray.length) {
+        }
+        if (secondArray.length != firstArray.length) {
             return false;
         } //не равна длина массива first и second возвращаем false
 
-        for (int i = 0; i < length; i++) {    //сравниваем значения массивов
+        for (int i = 0; i < firstArray.length; i++) {    //сравниваем значения массивов
             if (firstArray[i] != secondArray[i]) {
             return false;
             }
@@ -20,17 +17,14 @@ public class CompareArrays {
         return true;
     }
 
-
     public static void main(String[] args) {
         int[] firstArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         int[] secondArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         int[] thirdArray = new int[]{3, 2, 3, 3, 2, 2, 2, 9,7};
         int[] anotherArray = new int[]{1, 2, 3, 4, 5, -222, 7, 8};
 
-        System.out.println(Compare(firstArray, secondArray));
-        System.out.println(Compare(secondArray, thirdArray));
-        System.out.println(Compare(anotherArray,firstArray));
-
-
+        System.out.println(compareArrays(firstArray, secondArray));
+        System.out.println(compareArrays(secondArray, thirdArray));
+        System.out.println(compareArrays(anotherArray,firstArray));
     }
 }
