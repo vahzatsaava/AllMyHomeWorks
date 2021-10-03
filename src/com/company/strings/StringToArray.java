13d0c43aeee8459249f5639f8554c35cc2159120
Vahza <waja11@mail.ru>
2021-10-03 23:49:40 +0300

    третий залив String

--- src/com/company/strings/StringToArray.java
+++ src/com/company/strings/StringToArray.java
@@ -5,26 +5,34 @@ import java.util.Arrays;
 public class StringToArray {
     //    Дана строка, состоящая из слов. Слова разделены пробелами. Преобразовать строку в массив слов.
     private static void stringToArrray(String example) {
-        char[] result = example.toCharArray();
-/*
-
-//        for (int i = 0; i < result.length; i++) {  //создаем массив char и переводим нашу переменную example в char
-
-//            if (result[i] != ' ') {                // если result[i] не равен "пробелу" , то выводим result[i]
-//                System.out.println(result[i]);
-//            }
-//
-//        }
-
-
- */  // решение с for
-        for (char i : result) {
-            if (i != ' ') {
-                System.out.println(i);
+        int count = 0;
+        example.split(example);
+        //   нарисовать блок схему
+        // 1) извлечь каждое слово из строки
+        // 2) передать извлеченные слова в масси(записать)
+        // 3) _.._вернуть этот массив
+        for (int i = 0; i < example.length(); i++) {
+            if (example.charAt(i) == ' ') {
+                count++;
             }
+        }
+        String[] stringArray = new String[count + 1];
+        String result = "";
+
+        for (int i = 0; i < count + 1; i++) {
+             if (example.charAt(i) != ' ')
+             {
+                    result = result + example.charAt(i) ;
+              }
+          //  System.out.println(stringArray[i]=result);
+            stringArray[i]=result;
+        }
+        for (String i:stringArray) {
+            System.out.println(i);
         }
 
     }
+
     public static void main(String[] args) {
         String nameString = "Hello Dear Friends";
         stringToArrray(nameString);
