6fad17522a6e46b6cc5ddd9e830e1997a9e88af1
Vahza <waja11@mail.ru>
2021-10-06 19:10:44 +0300

    Александр не пойму как их сравнить они разной длины может массив чаров нужен?

--- src/com/company/strings/ExtractSubstring.java
+++ src/com/company/strings/ExtractSubstring.java
@@ -3,16 +3,54 @@ package com.company.strings;
 import java.util.Scanner;
 
 public class ExtractSubstring {
-    //  Извлечь подстроку из строки (пример -> строка: "привет, друг!"" подстрока: "ет," результат: "прив друг!")
-    private static void extractSubString(String example, int leftBorder, int rightBorder) {
-        String builderSub; // переменная для вырезания строки
-        String builderLast = "";
+    //  удалить подстроку из строки (пример -> строка: "привет, друг!"" подстрока: "ет," результат: "прив друг!")
+    // найти подстроку ет,(любую переданную строку)
+    private static boolean contains(String stringMain, String stringSub) {
 
-        for (int i = 0; i < example.length(); i++) {
-            builderSub = example.substring(leftBorder, rightBorder); // вырезанные значения
-            builderLast = example.replace(builderSub, ""); // удаляем переменню builderSub
+        boolean result = true;
+
+        if (stringMain.equals("")) {
+            result = false;
+        } else if (stringSub == null && stringMain == null) {
+            return result = false;
+        } else if (stringSub.length() == 0 && stringSub == "") {
+            result = true;
+        } else if (stringSub.length() > stringMain.length()) {
+            result = false;
         }
-        System.out.println(builderLast); //выводим на консоль измененную строку
+
+        for (int i = 0; i < stringMain.length(); i++) {
+            if (stringMain.charAt(i)!=stringSub.charAt(i)){ // не пойму как сравнить тут
+                // значения ,если они разной длины ,надо ли использовать массив
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+    private static String string(String result, String value) {
+        String lastResult = "";
+        for (int i = 0; i < result.length(); i++) {
+            if (result != value) {
+                lastResult += result.charAt(i);
+            } else {
+                lastResult += result.charAt(i) + value;
+            }
+        }
+        return lastResult;
+    }
+
+    private static String stringSubstring(String example, int leftValue, int rightValue) {
+        String result = "";
+
+        for (int i = leftValue; i <= rightValue; i++) {
+            result += example.charAt(i);
+
+        }
+
+        return result;
+
     }
 
     // ввод пользователем значений
@@ -20,16 +58,23 @@ public class ExtractSubstring {
         System.out.println("Введите параметры ");
         Scanner scanner = new Scanner(System.in);
         int result = scanner.nextInt();
-        while (result < 0 ) {
+        while (result < 0) {
             System.out.println("Введите параметры еще раз ");
             result = scanner.nextInt();
         }
         return result;
     }
+
     public static void main(String[] args) {
         String nameString = "Hello Dear Friend";
-        int left = returnInput();
-        int right = returnInput();
-        extractSubString(nameString, left, right);
+        //  int left = returnInput();
+        // int right = returnInput();
+        //nameString=   string("hello","rt");
+        //  System.out.println(nameString);
+        nameString = stringSubstring("Hello ", 1, 4);
+        System.out.println(nameString);
+
+        boolean isSub = contains("Hello", "lo");
+        System.out.println(isSub);
     }
 }
