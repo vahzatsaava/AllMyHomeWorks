072acf1df58eb9b002f89383f670cd0069aea8ec
Vahza <waja11@mail.ru>
2021-10-13 15:36:42 +0300

    Всавтьте массив в position

--- src/com/company/arraysHighLevel/ArrayIntoArray.java
+++ src/com/company/arraysHighLevel/ArrayIntoArray.java
@@ -4,42 +4,46 @@ import java.util.Random;
 
 public class ArrayIntoArray {
     // Даны два массива, вставить второй массив в первый на указанное место
-    private static int[] pasteArrayInPosition(int[] mainArray, int[] pastedArray, int position) {
-        int[] pastedArrays = new int[mainArray.length + pastedArray.length];
-        for (int i = 0; i < mainArray.length; i++) {
+    //вставить на позицию
+    private static int[] pastedArrays;
+    private static int[] resultArray;
 
+    private static int[] pasteArrayInPosition(int[] mainArray, int[] subArray) {
+        pastedArrays = new int[mainArray.length + subArray.length];
+
+        //   for (int i = 0; i < subArray.length; i++) {
+        //      pastedArrays[position + i] = subArray[i];
+        //   }
+
+        for (int i = 0; i < mainArray.length; i++) {
             pastedArrays[i] = mainArray[i];
         }
+
         return pastedArrays;
     }
 
+    private static int[] pasteSubPosition(int[] subArray, int position) {
+        resultArray = new int[pastedArrays.length];
+        return resultArray;
+    }
 
     public static void main(String[] args) {
-        int[] mainArray = new int[]{2, -2, 1, 45, 54 - 7};
-        int[] pastedArray = new int[]{3, 2, 0};
-
-        //        int[] newArray = new int[mainArray.length+pastedArray.length];
-//        int[] newSecond = new int[mainArray.length+pastedArray.length];
-//
-//        for (int i = 0; i <mainArray.length ; i++) {
-//            newArray[i] = mainArray[i];
-//        }
-//        for (int i = 0; i < newArray.length; i++) {
-//            newSecond[5] = pastedArray[i];
-//
-//        }
-//
-//        for (int i:newSecond
-//             ) {
-//            System.out.println(i);
-//
-//        }
-        int[] array = pasteArrayInPosition(mainArray, pastedArray, 0);
+        int[] mainArray = new int[]{2, -2, 1, 45, 54, -7};
+        int[] subArray = new int[]{3, 2, 1};
+        int[] array = pasteArrayInPosition(mainArray, subArray);
         for (int i : array
         ) {
             System.out.println(i);
 
         }
+        System.out.println();
+        int[] arrayResult = pasteSubPosition(subArray, 1);
+        for (int i : arrayResult
+        ) {
+            System.out.println(i);
+
+        }
+
 
     }
 }
