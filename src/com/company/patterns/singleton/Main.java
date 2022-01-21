65211462d6bb4ea33bc5d145b619a39e023fc244
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2022-01-21 11:22:04 +0300

    added Singleton pattern

---
+++ src/com/company/patterns/singleton/Main.java
@@ -0,0 +1,11 @@
+package com.company.patterns.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
---
+++ src/com/company/patterns/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package com.company.patterns.singleton;
+
+public class Singleton {
+    private static Singleton singleton;
+
+    public static Singleton getInstance(){
+        if (singleton == null){
+            singleton = new Singleton();
+        }
+
+        return singleton;
+    }
+}
