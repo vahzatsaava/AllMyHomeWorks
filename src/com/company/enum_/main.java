6a6e485f76752e025c8e31cb7a57e6b7c1576510
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2022-01-26 15:50:01 +0300

    added enum

---
+++ src/com/company/enum_/main.java
@@ -0,0 +1,23 @@
+package com.company.enum_;
+
+public class main {
+    public static void main(String[] args) {
+        if (getDay() == DaysOfWeek.MONDAY){
+            System.out.println("Понедельник");
+        }
+        if (getDay() == DaysOfWeek.HURRAY){
+
+        }
+    }
+
+    private static DaysOfWeek getDay(){
+        int a = 5;
+
+        if (a == 5){
+            return DaysOfWeek.MONDAY;
+        }
+        else{
+            return DaysOfWeek.HURRAY;
+        }
+    }
+}
