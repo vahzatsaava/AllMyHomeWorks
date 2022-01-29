42eee325090aaf52cc8a804e099b848c8e99a6b6
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2022-01-29 12:56:13 +0300

    refactor

--- src/com/company/enum_/main.java
+++ src/com/company/enum_/main.java
@@ -1,6 +1,18 @@
 package com.company.enum_;
 
 public class main {
+    class Car{
+        String eng;
+        String tur;
+
+        public void setEng(String eng) {
+            this.eng = eng;
+        }
+
+        public void setTur(String tur) {
+            this.tur = tur;
+        }
+    }
     public static void main(String[] args) {
         if (getDay() == DaysOfWeek.MONDAY){
             System.out.println("Понедельник");
@@ -8,9 +20,13 @@ public class main {
         if (getDay() == DaysOfWeek.HURRAY){
 
         }
+
+
+
     }
 
     private static DaysOfWeek getDay(){
+
         int a = 5;
 
         if (a == 5){
